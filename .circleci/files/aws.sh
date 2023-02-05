61c9859fe0637f199cf084b6fc9d9a821c07939f
AhmedMattar21 <ahmedmattar1998@gmail.com>
2023-02-05 19:32:40 +0200

    Deploy Infrastructure

--- .circleci/files/aws.sh
+++ .circleci/files/aws.sh
@@ -1,1 +1,1 @@
-aws cloudformation deploy --template-file $1 --stack-name $2 --parameter-overrides WorkflowID=s3-bucket
+aws cloudformation deploy --template-file $1 --stack-name $2 --parameter-overrides WorkflowID=abcdefg
