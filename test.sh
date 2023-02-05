91ef0f06faf81a07837d6ea9215d05b44cea2dba
AhmedMattar21 <ahmedmattar1998@gmail.com>
2023-02-05 21:28:32 +0200

    Cleanup | fix 3

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
-OldWorkflowID=$(curl https://kvdb.io/${KVDB_BUCKET}/old_workflow_id)
+export OLD_WORKFLOW_ID=abcw3
+echo "Old Workflow ID: $OLD_WORKFLOW_ID"
+curl https://kvdb.io/${KVDB_BUCKET}/old_workflow_id2 -d ${OLD_WORKFLOW_ID}
+echo "should be set on kv"
+
+OldWorkflowID=$(curl https://kvdb.io/${KVDB_BUCKET}/old_workflow_id2)
 CurrentWorkflowID=${CIRCLE_WORKFLOW_ID:0:7}
 echo "DEBUG=$OldWorkflowID"
 echo "DEBUG2=" $OldWorkflowID
